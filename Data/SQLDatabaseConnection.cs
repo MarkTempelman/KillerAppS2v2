840aaed9bf620f3e528811f5211fab2affee4dde
MarkTempelman <43167879+MarkTempelman@users.noreply.github.com>
2019-10-09 10:09:30 +0200

    Set-up multi layer structure

---
+++ Data/SQLDatabaseConnection.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+
+namespace Data
+{
+    class SQLDatabaseConnection
+    {
+        public static MySqlConnection GetConnection()
+        {
+            return new MySqlConnection("Server=studmysql01.fhict.local;Uid=dbi415127;Database=dbi415127;Pwd=test1234;");
+        }
+    }
+}
