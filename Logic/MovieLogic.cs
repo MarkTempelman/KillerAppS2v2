c40f4031f8e1bb54123d2199ef38cb19f8ceab51
Mark Tempelman <43167879+MarkTempelman@users.noreply.github.com>
2019-10-21 10:04:35 +0200

    Added part of the Search functionality
    Added part of the Search functionality

--- Logic/MovieLogic.cs
+++ Logic/MovieLogic.cs
@@ -11,19 +11,44 @@ namespace Logic
 {
     public class MovieLogic
     {
-        private readonly IMovieContext _iMovieContext = new MovieSQLContext();
+        public MovieLogic()
+        {
+            _iMovieContext = new MovieSQLContext();
+        }
+
+        public MovieLogic(IMovieContext movieContext)
+        {
+            _iMovieContext = movieContext;
+        }
+        private readonly IMovieContext _iMovieContext;
         public List<MovieModel> GetAllMovies()
         {
             return _iMovieContext.GetAllMovies();
         }
 
-        public List<MovieModel> SearchForMovies(SearchModel search)
+        public List<MovieModel> SearchForMovies(List<MovieModel> movies, SearchModel search)
         {
-            List<MovieModel> movies = GetAllMovies();
-            
             return movies;
         }
 
-      
+        private List<MovieModel> FilterByGenre(List<MovieModel> movies, GenreModel genre)
+        {
+            return (from movie in movies from movieGenre in movie.Genres where movieGenre.Genre == genre.Genre select movie).ToList();
+        }
+
+        private List<MovieModel> ReleasedAfter(List<MovieModel> movies, DateTime releasedAfter)
+        {
+            return movies.Where(m => m.ReleaseDate > releasedAfter).ToList(); ;
+        }
+
+        private List<MovieModel> ReleasedBefore(List<MovieModel> movies, DateTime releasedBefore)
+        {
+            return movies.Where(m => m.ReleaseDate < releasedBefore).ToList();
+        }
+
+        private List<MovieModel> SearchForMoviesByTitle(List<MovieModel> movies, string searchTerm)
+        {
+            return (movies.Where(movie => movie.Title.Contains(searchTerm))).ToList();
+        }
     }
 }
--- Models/MovieModel.cs
+++ Models/MovieModel.cs
@@ -12,7 +12,7 @@ namespace Models
         public string Title { get; set; }
         public string Description { get; set; }
         public DateTime ReleaseDate { get; set; }
-        public string Genre { get; set; }
+        public List<GenreModel> Genres { get; set; }
 
         public MovieModel(int movieId, string title, string description, DateTime releaseDate)
         {
