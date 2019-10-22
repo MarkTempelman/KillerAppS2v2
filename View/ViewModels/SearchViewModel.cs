6088bca3140a5a3070cda72631e4795f51d35b98
MarkTempelman <43167879+MarkTempelman@users.noreply.github.com>
2019-10-22 10:35:37 +0200

    Improved HTML
    Improved HTML for better looks

--- View/ViewModels/SearchViewModel.cs
+++ View/ViewModels/SearchViewModel.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -10,11 +11,19 @@ namespace View.ViewModels
     {
         [DisplayName("Sort by:")]
         public string SortBy { get; set; }
+
         public GenreViewModel Genre { get; set; }
+
         [DisplayName("Released after:")]
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:dd/MM/yyyy}", ApplyFormatInEditMode = true)]
         public DateTime ReleasedAfter { get; set; } = DateTime.MinValue;
+
         [DisplayName("Released before:")]
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:dd/MM/yyyy}", ApplyFormatInEditMode = true)]
         public DateTime ReleasedBefore { get; set; } = DateTime.MaxValue;
+
         [DisplayName("Search")]
         public string SearchTerm { get; set; }
 
