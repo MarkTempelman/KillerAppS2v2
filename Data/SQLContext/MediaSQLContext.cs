5fb57568514550d83e7f7c773d80a32c187ee17d
Mark Tempelman <43167879+MarkTempelman@users.noreply.github.com>
2019-11-26 14:28:26 +0100

    Added GetMediaIdFromMovieId

--- Data/SQLContext/MediaSQLContext.cs
+++ Data/SQLContext/MediaSQLContext.cs
@@ -2,14 +2,39 @@
 using System.Collections.Generic;
 using System.Text;
 using Data.Interfaces;
+using MySql.Data.MySqlClient;
 
 namespace Data.SQLContext
 {
     public class MediaSQLContext : IMediaContext
     {
+        private readonly MySqlConnection _conn = SQLDatabaseConnection.GetConnection();
         public int GetMediaIdFromMovieId(int movieId)
         {
-            return 0;
+            try
+            {
+                int mediaId = -1;
+                MySqlCommand command = new MySqlCommand("SELECT MediaId FROM movie WHERE MovieId = @movieId", _conn);
+                command.Parameters.AddWithValue("@movieId", movieId);
+
+                _conn.Open();
+                MySqlDataReader reader = command.ExecuteReader();
+                while (reader.Read())
+                {
+                    mediaId = reader.GetInt32("MediaId");
+                }
+
+                return mediaId;
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                throw;
+            }
+            finally
+            {
+                _conn.Close();
+            }
         }
     }
 }
