190bf56028851168cbd6b201fcfccbd5248a237b
Mark Tempelman <43167879+MarkTempelman@users.noreply.github.com>
2019-11-28 10:38:12 +0100

    Added extra unit tests

--- LogicTests/MovieLogicTests.cs
+++ LogicTests/MovieLogicTests.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using Data.Interfaces;
+using Enums;
 using LogicTests.MemoryContext;
 using Logic;
 using Logic.Models;
@@ -19,6 +20,8 @@ namespace LogicTests
         private SearchLogic _searchLogic;
         private MovieModel _testMovie1;
         private MovieModel _testMovie2;
+        private GenreModel _testGenre1;
+        private GenreModel _testGenre2;
         private SearchModel _search;
 
 
@@ -30,21 +33,62 @@ namespace LogicTests
             _genreLogic = new GenreLogic(_iGenreContext);
             _searchLogic = new SearchLogic(_genreLogic);
             _movieLogic = new MovieLogic(_iMovieContext, _genreLogic, _searchLogic);
+            _testGenre1 = new GenreModel("test1", 1);
+            _testGenre2 = new GenreModel("test2", 2);
             _testMovie1 = new MovieModel(1, "TestTitle2", "TestDescription1", new DateTime(2019, 10, 23), 1);
             _testMovie2 = new MovieModel(2, "TestTitle1", "TestDescription2", new DateTime(2019, 10, 15), 2);
         }
 
         [Test]
-        public void GetAllMoviesTest()
+        public void TC01GetAllMoviesTest()
         {
-            IEnumerable<int> expected = new List<int>
-            {
-                _testMovie1.MovieId,
-                _testMovie2.MovieId
-            };
+            IEnumerable<int> expected = new List<int> {1, 2};
             List<int> actual = _movieLogic.GetAllMovies().Select(movie => movie.MovieId).ToList();
 
             Assert.AreEqual(expected, actual);
+        }
+
+        [Test]
+        public void TC03SortByReleaseDate()
+        {
+            _search = new SearchModel {SortBy = SortBy.Date};
+            List<int> expected = new List<int> {2, 1};
+
+            List<int> actual = _movieLogic.GetMoviesBySearchModel(_search).Select(movie => movie.MovieId).ToList();
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [Test]
+        public void TC04SortByTitle()
+        {
+            _search = new SearchModel { SortBy = SortBy.Title };
+            List<int> expected = new List<int> { 2, 1 };
+
+            List<int> actual = _movieLogic.GetMoviesBySearchModel(_search).Select(movie => movie.MovieId).ToList();
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [Test]
+        public void TC05FilterByDate()
+        {
+            _search = new SearchModel
+            {
+                ReleasedAfter = new DateTime(2019, 10, 16),
+                ReleasedBefore = new DateTime(2019, 10, 25)
+            };
+
+            var actual = _movieLogic.GetMoviesBySearchModel(_search);
+            
+            Assert.Multiple(() =>
+                {
+                    foreach (var movie in actual)
+                    {
+                        Assert.Greater(movie.ReleaseDate, new DateTime(2019, 10, 16));
+                        Assert.Less(movie.ReleaseDate, new DateTime(2019, 10, 25));
+                    }
+                });
         }
 
         [Test]
