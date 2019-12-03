ed8e461eaf337388d1661cd7915ab06fbcc07f35
MarkTempelman <43167879+MarkTempelman@users.noreply.github.com>
2019-12-03 12:53:45 +0100

    Part of View for favourites

--- View/Controllers/PlaylistController.cs
+++ View/Controllers/PlaylistController.cs
@@ -5,16 +5,19 @@ using System.Security.Claims;
 using System.Threading.Tasks;
 using Logic;
 using Microsoft.AspNetCore.Mvc;
+using View.Helpers;
 
 namespace View.Controllers
 {
     public class PlaylistController : Controller
     {
         private readonly PlaylistLogic _playlistLogic;
+        private readonly MovieLogic _movieLogic;
 
-        public PlaylistController(PlaylistLogic playlistLogic)
+        public PlaylistController(PlaylistLogic playlistLogic, MovieLogic movieLogic)
         {
             _playlistLogic = playlistLogic;
+            _movieLogic = movieLogic;
         }
 
         public IActionResult AddMovieToFavourites(int id)
@@ -22,6 +25,12 @@ namespace View.Controllers
             _playlistLogic.AddMovieToFavourites(id, int.Parse(User.Claims.FirstOrDefault(c => c.Type == System.Security.Claims.ClaimTypes.Sid).Value));
 
             return RedirectToAction("Index", "Movie");
+        }
+
+        public IActionResult Favourites()
+        {
+            return View(ModelToViewModel.ToMovieViewModels(_movieLogic.GetMoviesFromFavourites(int.Parse(User.Claims
+                .FirstOrDefault(c => c.Type == System.Security.Claims.ClaimTypes.Sid).Value))));
         }
     }
 }
