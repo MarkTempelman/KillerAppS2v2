55794c0edabef30e0ae3496bac9b05500a2d2496
MarkTempelman <43167879+MarkTempelman@users.noreply.github.com>
2019-12-15 12:24:18 +0100

    Added user tests

---
+++ LogicTests/MemoryContext/UserMemoryContext.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Data.DTO;
+using Data.Interfaces;
+using Enums;
+
+namespace LogicTests.MemoryContext
+{
+    public class UserMemoryContext : IUserContext
+    {
+        public void CreateUser(UserDTO user)
+        {
+            throw new NotImplementedException();
+        }
+
+        public UserDTO GetUserByUsername(string username)
+        {
+            UserDTO user = new UserDTO();
+            if (username == "Admin")
+            {
+                user = new UserDTO(
+                    "Admin",
+                    "admin@admin.com",
+                    AccountType.Admin,
+                    "$2b$10$7iFKR8mNWQ8TPiOYYq1pzeBM.TQ4A9NMQl0iM7MVocQsNCUyhBgkS"
+                )
+                {
+                    UserId = 1
+                };
+            }
+            if (username == "User")
+            {
+                user = new UserDTO(
+                    "User",
+                    "user@user.com",
+                    AccountType.User,
+                    "$2b$10$GKMANNUgilRJ39yD4BjQwOq5w3OETc7rMkeeLadjfFbKTdhNRP78C"
+                )
+                {
+                    UserId = 2
+                };
+            }
+
+            return user;
+        }
+
+        public bool DoesUsernameExist(string username)
+        {
+            return username == "Admin" || username == "User";
+        }
+
+        public bool DoesEmailAddressExist(string emailAddress)
+        {
+            return emailAddress == "admin@admin.com" || emailAddress == "user@user.com";
+        }
+    }
+}
---
+++ LogicTests/UserTests.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Data.Interfaces;
+using Logic;
+using LogicTests.MemoryContext;
+using NUnit.Framework;
+
+namespace LogicTests
+{
+    public class UserTests
+    {
+        private IUserContext _iUserContext;
+        private UserLogic _userLogic;
+        [SetUp]
+        public void SetUp()
+        {
+            _iUserContext = new UserMemoryContext();
+            _userLogic = new UserLogic(_iUserContext);
+        }
+
+        [Test]
+        public void CheckUserValidity_CorrectUsernameAndPassword_ReturnsValidUserModel()
+        {
+            var expected = 1;
+            var actual = _userLogic.CheckUserValidity("Admin", "admin").UserId;
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [Test]
+        public void CheckUserValidity_WrongPassword_ReturnsNull()
+        {
+            var actual = _userLogic.CheckUserValidity("Admin", "aefae");
+
+            Assert.IsNull(actual);
+        }
+
+        [Test]
+        public void CheckUserValidity_NonExistantUser_ReturnsNull()
+        {
+            var actual = _userLogic.CheckUserValidity("aefaef", "password");
+
+            Assert.IsNull(actual);
+        }
+
+        [Test]
+        public void DoesUserNameExist_ExistingUserName_ReturnsTrue()
+        {
+            var actual = _userLogic.DoesUserNameExist("Admin");
+
+            Assert.IsTrue(actual);
+        }
+
+        [Test]
+        public void DoesUserNameExist_NonExistingUserName_ReturnsFalse()
+        {
+            var actual = _userLogic.DoesUserNameExist("Admin2");
+
+            Assert.IsFalse(actual);
+        }
+
+        [Test]
+        public void DoesEmailAddressExist_ExistingEmail_ReturnsTrue()
+        {
+            var actual = _userLogic.DoesEmailAddressExist("admin@admin.com");
+
+            Assert.IsTrue(actual);
+        }
+
+        [Test]
+        public void DoesEmailAddressExist_NonExistingEmail_ReturnsFalse()
+        {
+            var actual = _userLogic.DoesEmailAddressExist("admin2@admin.com");
+
+            Assert.IsFalse(actual);
+        }
+    }
+}
