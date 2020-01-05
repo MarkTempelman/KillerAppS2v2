735b94ce3892e37b49897d7e2e93f57603d3b43a
MarkTempelman <43167879+MarkTempelman@users.noreply.github.com>
2020-01-05 13:42:46 +0100

    Added admin  integration tests

---
+++ IntegrationTests/AdminIntegrationTests.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Reflection;
+using System.Text;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace IntegrationTests
+{
+    class AdminIntegrationTests
+    {
+        private IWebDriver _driver;
+        public string _homeURL;
+
+        [SetUp]
+        public void Setup()
+        {
+            _homeURL = "https://localhost:44359/";
+            _driver = new ChromeDriver(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location));
+        }
+
+        [Test]
+        public void AddMovie()
+        {
+            TestHelpers.LoadHome(_driver);
+
+            _driver.FindElement(By.Id("NavLogin")).Click();
+            TestHelpers.Login(_driver, "Admin", "admin");
+
+            _driver.FindElement(By.Id("NavAddMovie")).Click();
+
+            var guid = Convert.ToBase64String(Guid.NewGuid().ToByteArray());
+            guid = guid.Remove(guid.Length - 2);
+
+            TestHelpers.AddMovie(_driver, guid);
+
+            Assert.True(_driver.PageSource.Contains(guid));
+        }
+
+        [Test]
+        public void AddMovieThenEdit()
+        {
+            var guid = Convert.ToBase64String(Guid.NewGuid().ToByteArray());
+            guid = guid.Remove(guid.Length - 2);
+
+            TestHelpers.LoadHome(_driver);
+            _driver.FindElement(By.Id("NavLogin")).Click();
+            TestHelpers.Login(_driver, "Admin", "admin");
+
+            _driver.FindElement(By.Id("NavAddMovie")).Click();
+            TestHelpers.AddMovie(_driver, guid);
+
+            _driver.FindElement(By.Id("Edit " + guid)).Click();
+            TestHelpers.EditMovie(_driver, guid);
+
+            Assert.True(_driver.PageSource.Contains(
+                guid + " This movie was generated and edited by the automated testing system."));
+        }
+
+        [Test]
+        public void Temp()
+        {
+            TestHelpers.LoadHome(_driver);
+
+            _driver.FindElement(By.Id("NavLogin")).Click();
+            TestHelpers.Login(_driver, "Admin", "admin");
+
+            _driver.FindElement(By.Id("Edit Avengers: Infinity War")).Click();
+
+            //var movieTitleElement = _driver.FindElement(By.LinkText("Avengers: Infinity War"));
+            //movieTitleElement = movieTitleElement.FindElement(By.XPath("./parent::"));
+            //movieTitleElement = movieTitleElement.FindElement(By.XPath("./parent::"));
+            //movieTitleElement = movieTitleElement.FindElement(By.XPath("./parent::"));
+
+            //movieTitleElement.Click();
+
+        }
+
+        [TearDown]
+        public void TearDownTest()
+        {
+            _driver.Close();
+            _driver.Dispose();
+        }
+    }
+}
