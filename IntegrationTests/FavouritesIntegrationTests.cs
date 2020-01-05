cae76b1b851fd004eda3e4b36f5639ccbb24605e
MarkTempelman <43167879+MarkTempelman@users.noreply.github.com>
2020-01-05 12:17:14 +0100

    Added Favourites integration tests

---
+++ IntegrationTests/FavouritesIntegrationTests.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Reflection;
+using System.Text;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace IntegrationTests
+{
+    class FavouritesIntegrationTests
+    {
+        private IWebDriver _driver;
+        public string _homeURL;
+
+        [SetUp]
+        public void Setup()
+        {
+            _homeURL = "https://localhost:44359/";
+            _driver = new ChromeDriver(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location));
+        }
+
+        [Test]
+        public void AddMovieToFavouritesThenRemoveFromFavourites()
+        {
+            TestHelpers.LoadHome(_driver);
+
+            _driver.FindElement(By.Id("NavLogin")).Click();
+            TestHelpers.Login(_driver, "User", "user");
+
+            _driver.Navigate().GoToUrl("https://localhost:44359/Playlist/AddMovieToFavourites/1");
+
+            _driver.FindElement(By.Id("NavFavourites")).Click();
+
+            Assert.True(_driver.PageSource.Contains("Avengers"));
+
+            _driver.Navigate().GoToUrl("https://localhost:44359/Playlist/RemoveMovieFromFavourites/1");
+            _driver.FindElement(By.Id("NavFavourites")).Click();
+
+            Assert.False(_driver.PageSource.Contains("Avengers"));
+        }
+
+        [TearDown]
+        public void TearDownTest()
+        {
+            _driver.Close();
+            _driver.Dispose();
+        }
+    }
+}
